d58f0ed58d1fa42bc484d8e3d71c3ac5c6fa2a25
JavokhirAbdurazzokov <168398916+JavokhirAbdurazzokov@users.noreply.github.com>
2024-07-13 13:13:31 +0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,13 @@
+let son=+prompt("son kiriting:")
+
+
+
+if(son%3==0){
+    alert("Fizz")
+}else if(son%5==0){
+    alert("Buzz")
+}else if(son%15==0){
+    alert("FizzBuzz")
+}else{
+    alert("Fizz ham Buuz ham emas")
+}
